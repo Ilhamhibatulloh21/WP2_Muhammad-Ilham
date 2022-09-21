d609bd6fcc9cf003b814b2e135881da0ae1b883b
unknown <ilhamhibatulloh0201@gmail.com>
2022-09-21 19:27:37 +0700

    pustaka-booking

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Muhammad Ilham
+            Saya tingga di daerah Karawang
+            olah raga yang saya sukai adalah
+            Futsal";
+    }
+}
